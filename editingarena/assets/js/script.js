889f5eac80661d48868683ac8fd6eb00bd85fc5a
Nadeem Ahmad <workingnadeem@gmail.com>
2024-09-05 19:50:54 +0500

    Delete editingarena directory

--- editingarena/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const burger = document.querySelector('.navbar-burger');
-    const collapseMenu = document.querySelector('.collapse-menu');
-
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('active');
-        collapseMenu.classList.toggle('active');
-    });
-});
