ac4db0c0c895b229aa4ff4c7fa6685b6163a105a
Nadeem Ahmad <workingnadeem@gmail.com>
2024-09-12 04:33:39 +0500

    Add files via upload

---
+++ editingarena/assets/data/videos.js
@@ -0,0 +1,130 @@
+export const video = [
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/COqCoNpqJuo?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="Magnates Media" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 1,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/zeOw4hZNLG8?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="Magnates Media" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 1,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/_8FbkjQp36M?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="Magnates Media" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 1,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/tlCJWgBGuvk?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="Cashcow" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 2,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/r8Hza6lqCeY?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="CashCow" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 2,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/257F-DGUIgU?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="CashCow" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 2,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/XucgPc1wJvA?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="CashCow" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 2,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/M_6oOsmK020?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="Ali Abdaal" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 3,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/WhADbvWYH2E?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="Ali Abdaal" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 3,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/wi6c_xgGYr4?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="Ali Abdaal" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 3,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/bCCmlVcUQ-M?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="Ali Abdaal" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 3,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/CksWkDIW72w?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="Iman Gadzhi" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 4,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/Hl2nM2VZ8j8?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="Custom Style" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 5,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/qms9XH6qXgY?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="Custom Style" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 5,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/PioKZNnHTCk?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="Custom Style" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 5,
+  },
+  {
+    link: `
+                <iframe class="carousel-container" src="https://www.youtube.com/embed/NwpL6b8OoIs?list=PLh1UdpQcCM9dupMx5pYkExlAgXAHEG56y"
+                title="Custom Style" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+                picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                    `,
+    style: 5,
+  },
+];
