db865d621629639f9dd2d479f291cb3706e84ed5
Nadeem Ahmad <workingnadeem@gmail.com>
2024-09-23 23:36:21 +0500

    Add files via upload

---
+++ RNS/assets/data/categories.js
@@ -0,0 +1,22 @@
+export const images = [
+  {
+    src: "assets/imgs/inner-img-10.jpg",
+    design: "Handmade",
+    category: "Bespoke",
+  },
+  {
+    src: "assets/imgs/about-gallery-img-3.jpg",
+    design: "Modern",
+    category: "Luxury",
+  },
+  {
+    src: "assets/imgs/main-home-img-2.jpg",
+    design: "Vintage",
+    category: "Classic",
+  },
+  {
+    src: "assets/imgs/main-home-img-14.jpg",
+    design: "Contemporary",
+    category: "Formal",
+  },
+];
