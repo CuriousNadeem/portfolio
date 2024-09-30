012e96b4bd469fb057a063ecdedd4186d2bed522
Nadeem Ahmad <workingnadeem@gmail.com>
2024-10-01 02:37:42 +0500

    Add files via upload

---
+++ RNS/assets/data/hero-carousel.js
@@ -0,0 +1,11 @@
+export const carouselImgs = [
+  {
+    link: "assets/imgs/main-home-01.webp",
+  },
+  {
+    link: "assets/imgs/main-home-02.webp",
+  },
+  {
+    link: "assets/imgs/main-home-03.webp",
+  },
+];
