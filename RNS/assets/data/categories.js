1982af5b5fb4580843906c64cb483e310f30c27e
Nadeem Ahmad <workingnadeem@gmail.com>
2024-11-01 22:21:16 +0500

    Add files via upload

---
+++ RNS/assets/data/categories.js
@@ -0,0 +1,27 @@
+export const images = [
+  {
+    src: "assets/imgs/flowergirl.jpg",
+    design: "Flower Girl Dresses",
+    category: "Flower Girl",
+  },
+  {
+    src: "assets/imgs/formal.jpg",
+    design: "Formal Gowns",
+    category: "Formals",
+  },
+  {
+    src: "assets/imgs/casual.jpg",
+    design: "Casual Dress",
+    category: "Casual",
+  },
+  {
+    src: "assets/imgs/party.jpg",
+    design: "Party Dress",
+    category: "Party",
+  },
+  {
+    src: "assets/imgs/office1.jpg",
+    design: "Office Dress",
+    category: "Office/Work",
+  },
+];
